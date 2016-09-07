b158b20cd2735cf23995ce578dd2ec2838fa7798
porcelli <alexandre.porcelli@gmail.com>
2016-09-07 14:44:19 -0400

    improve ranrom issue test (windows issue) using byteman instead of lucky thread.

--- uberfire-nio2-backport/uberfire-nio2-impls/uberfire-nio2-jgit/src/test/java/org/uberfire/java/nio/fs/jgit/ConcurrentJGitUtilTest.java
+++ uberfire-nio2-backport/uberfire-nio2-impls/uberfire-nio2-jgit/src/test/java/org/uberfire/java/nio/fs/jgit/ConcurrentJGitUtilTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 Red Hat, Inc. and/or its affiliates.
+ * Copyright 2016 Red Hat, Inc. and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,40 +16,34 @@
 
 package org.uberfire.java.nio.fs.jgit;
 
-import static org.junit.Assert.assertEquals;
-import static org.uberfire.java.nio.fs.jgit.util.JGitUtil.commit;
-
 import java.io.File;
 import java.io.IOException;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 
 import org.eclipse.jgit.api.Git;
+import org.jboss.byteman.contrib.bmunit.BMScript;
+import org.jboss.byteman.contrib.bmunit.BMUnitConfig;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.uberfire.java.nio.fs.jgit.util.JGitUtil;
-import org.uberfire.java.nio.fs.jgit.util.JGitUtil.JGitPathInfo;
 
+import static org.junit.Assert.*;
+import static org.uberfire.java.nio.fs.jgit.util.JGitUtil.*;
+
+@RunWith(org.jboss.byteman.contrib.bmunit.BMUnitRunner.class)
+@BMUnitConfig(loadDirectory = "target/test-classes", debug = true) // set "debug=true to see debug output
 public class ConcurrentJGitUtilTest extends AbstractTestInfra {
 
     @Test
+    @BMScript(value = "byteman/fail_on_treewalk.btm")
     public void testCommitAndResolve() throws IOException {
 
         // RHBPMS-4105
-        File parentFolder = createTempDirectory();
-        File gitFolder = new File( parentFolder, "mytest.git" );
+        final File parentFolder = createTempDirectory();
+        final File gitFolder = new File( parentFolder, "mytest.git" );
 
-        System.out.println( gitFolder.getAbsolutePath() );
-
-        Git git = JGitUtil.newRepository( gitFolder, true );
-
-        final Set<Integer> failureSet = new HashSet<Integer>();
-
-        final long start = System.currentTimeMillis();
+        final Git git = JGitUtil.newRepository( gitFolder, true );
 
         commit( git, "master", "name", "name@example.com", "1st commit", null, new Date(), false, new HashMap<String, File>() {
             {
@@ -62,58 +56,13 @@ public class ConcurrentJGitUtilTest extends AbstractTestInfra {
             }
         } );
 
-        Runnable commiter = new Runnable() {
-            @Override
-            public void run() {
-                System.out.println( "commiter start" );
-                try {
-                    for ( int i = 0; i < 200; i++ ) {
-                        final int fileNum = i;
-                        commit( git, "master", "name", "name@example.com", "commit with amend", null, new Date(), true, new HashMap<String, File>() {
-                            {
-                                put( "path/to/additional-file" + fileNum + ".txt", tempFile( "temp2222" ) );
-                            }
-                        } );
-                    }
-                } catch ( IOException ioe ) {
-                    ioe.printStackTrace();
-                }
-                System.out.println( "commiter finished : elapsedTime = " + (System.currentTimeMillis() - start) + " ms" );
-            }
-        };
-
-        Runnable reader = new Runnable() {
-            @Override
-            public void run() {
-                System.out.println( "reader start" );
-                for ( int i = 0; i < 2000; i++ ) {
-                    // Just want to get an existing file
-                    JGitPathInfo info = JGitUtil.resolvePath( git, "master", "path/to/file1.txt" );
-                    if ( info == null ) {
-                        System.out.println( "info == null, i = " + i );
-                        failureSet.add( i );
-                    } else if ( info.getPath() == null ) {
-                        System.out.println( "info.getPath() == null, i = " + i );
-                        failureSet.add( i );
-                    } else if ( !info.getPath().equals( "path/to/file1.txt" ) ) {
-                        System.out.println( "info.getPath() == " + info.getPath() + ", i = " + i );
-                        failureSet.add( i );
-                    }
-                }
-                System.out.println( "reader finished : elapsedTime = " + (System.currentTimeMillis() - start) + " ms" );
-            }
-        };
-
-        ExecutorService executor = Executors.newFixedThreadPool( 2 );
-        executor.execute( commiter );
-        executor.execute( reader );
-        executor.shutdown();
         try {
-            executor.awaitTermination( 300, TimeUnit.SECONDS );
-        } catch ( InterruptedException e ) {
-            e.printStackTrace();
+            JGitUtil.resolvePath( git, "master", "path/to/file1.txt" );
+            JGitUtil.resolvePath( git, "master", "path/to/file1.txt" );
+        } catch ( Exception ex ){
+            if (ex.getMessage().equals( "almost random failure" )){
+                fail();
+            }
         }
-
-        assertEquals( 0, failureSet.size() );
     }
 }
